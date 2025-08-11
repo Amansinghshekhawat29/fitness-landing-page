46a44f039dfc9c93c3be195946be70e86533a13a
Aman Singh Shekhawat <shekhawataman413@gmail.com>
2025-08-11 22:28:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 
 
 console.log("FitLife page loaded.");
+
+// Fade-in Scroll Animation
+const fadeElements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  fadeElements.forEach(el => {
+    const boxTop = el.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      el.classList.add('visible');
+    }
+  });
+});
